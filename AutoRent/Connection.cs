7d2c766e1516530afd3de11d4174f86f5053526b
Márk Életes <eletesmark@gmail.com>
2024-03-26 21:25:24 +0100

    Created Communication class (in Client) and GetAllCars method

---
+++ AutoRent/Connection.cs
@@ -0,0 +1,27 @@
+﻿using AutoRent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRent
+{
+    internal class Connection
+    {
+        public const string URL = "https://localhost:7204";
+
+        public Connection() { }
+
+        public List<Car> GetAllCars()
+        {
+            using (var wb = new WebClient())
+            {
+                var response = wb.DownloadString($"{URL}/getAllCars");
+                List<Car> carsList = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Car>>(response);
+                return carsList;
+            }
+        }
+    }
+}
