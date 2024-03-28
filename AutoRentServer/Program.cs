6e627e9225a6d79b55772c277c61b4929c7f14ad
Márk Életes <eletesmark@gmail.com>
2024-03-28 22:09:04 +0100

    login and getUser methods (server)

--- AutoRentServer/Program.cs
+++ AutoRentServer/Program.cs
@@ -7,6 +7,25 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+// LogIn
+app.MapPost("/login", (LoginUser login) =>
+{
+    AutorentContext _autorent = new AutorentContext();
+    User? user = _autorent.Users.FirstOrDefault(x => x.Username.Equals(login.username) && x.Password.Equals(login.password));
+
+    return Results.Json(user?.Id);
+});
+
+// GetUser
+app.MapGet("/getUser", (int id) =>
+{
+    AutorentContext _autorent = new AutorentContext();
+    User? user = _autorent.Users.FirstOrDefault(x => x.Id == id);
+
+    return JsonConvert.SerializeObject(user, Formatting.Indented, new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore }); ;
+});
+
+
 app.MapGet("/getAllCars", () =>
 {
     AutorentContext _autorent = new AutorentContext();
@@ -18,3 +37,13 @@ app.MapGet("/getAllCars", () =>
 });
 
 app.Run();
+
+#region Extra classes
+
+public class LoginUser
+{
+    public string username { get; set; }
+    public string password { get; set; }
+}
+
+#endregion
