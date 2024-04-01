171e3c9469d05491cd1815018cbd3550924ab6ab
Savitar233 <farkasmilanaron@gmail.com>
2024-04-01 16:52:43 +0200

    Login checks (button_login_Click)

--- AutoRent/AutorentLogin.cs
+++ AutoRent/AutorentLogin.cs
@@ -19,11 +19,25 @@ namespace AutoRent
 
         private void button_login_Click(object sender, EventArgs e)
         {
+            Connection con= new Connection();
+            if(textBox_username.TextLength==0 ||  textBox_password.TextLength==0)
+            {
+                MessageBox.Show("All fields are required!");
+                return;
+            }
+            int? loginResult=con.Login(textBox_username.Text,textBox_password.Text);
+            if(loginResult!=null)
+            { 
             // This will happen if login was successful :
             AutorentMain mainForm = new AutorentMain(this);
             this.Hide();
             mainForm.Show();
             mainForm.FormClosed += AutorentMain_FormClosed;
+            }
+            else
+            {
+                MessageBox.Show("Invalid username and/or password!");
+            }
         }
 
         private void AutorentMain_FormClosed(object sender, FormClosedEventArgs e)
