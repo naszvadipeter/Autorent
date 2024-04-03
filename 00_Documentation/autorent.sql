8d5deb779028c12dfb17ab4135117280626b5167
Életes Márk <74597812+eletesmark@users.noreply.github.com>
2024-04-03 15:19:07 +0200

    Added missing comma

--- 00_Documentation/autorent.sql
+++ 00_Documentation/autorent.sql
@@ -111,7 +111,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Tábla adatainak mentése autorent.users: ~1 rows (hozzávetőleg)
 INSERT INTO `users` (`id`, `username`, `name`, `password`) VALUES
 	(1, 'mark', 'Márk', '12345'),
-	(2, 'peter', 'Péter', '12345')
+	(2, 'peter', 'Péter', '12345'),
 	(3, 'milan', 'Milán', '12345');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
